4aa13f1a68990e50194b93b0e1e1e5f08802085e
feather2308 <127316591+feather2308@users.noreply.github.com>
2023-11-16 01:13:12 +0900

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,10 +1,7 @@
 # 할일
 '''
 초음파센서 주석처리 -> 안되면
-글자 폰트 수정하기 -> 일단 보고나서?
-버튼 코드 받기
-버튼 입력 받는 방법 보기
-버튼 입력 값의 사이 딜레이 필요함
+글자 폰트 수정하기 -> 일단 보고나서
 
 1. 가로세로 모드 생각해보기
 → 스텝모터 사용하기
@@ -14,11 +11,28 @@
 → 폰에서 제품 등록가능하게 구현용
 '''
 
+# 링크
+'''
+초음파센서
+https://rasino.tistory.com/349
+
+디스플레이
+https://www.instructables.com/Raspberry-Pi-Monitoring-System-Via-OLED-Display-Mo/
+https://github.com/adafruit/Adafruit_Python_SSD1306
+
+조이스틱
+https://github.com/mheidenreich/joystick/blob/main/joystick.py - 안될경우 참고하기
+https://youtu.be/fX225p-Sh58 - 영상
+
+스텝모터
+https://blog.naver.com/elepartsblog/221444212815
+'''
+
 # 시간, 스레드
 import time
 import threading
 
-# 초음파센서
+# 초음파센서, 조이스틱
 import RPi.GPIO as GPIO
 
 # 디스플레이
@@ -36,31 +50,33 @@ def paint():
     width = disp.width
     height = disp.height
 
+    # 글자 padding 및 갭 단위: px
+    padding = 2
+    text_gap = 20
+
     # 폰트 불러오기
     font = ImageFont.load_default()
 
+    # 폰트 불러올거면 쓰기.
+    #font = ImageFont.truetype('파일경로/파일명.ttf', 폰트크기)
+
+    # 영어원문. 혹시 몰라서 놔둠.
+    # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+    #font = ImageFont.truetype('Minecraftia.ttf', 8)
+
     while doing:
         if ultraSonic:
+            # 이미지 생성 1비트로 초기화
+            image = Image.new('1', (width, height))
+
+            # 그림 이미지드로우 객체로 가져오기
+            draw = ImageDraw.Draw(image)
+
+            # 배경 초기화
+            draw.rectangle((0,0,width,height), outline=0, fill=0)
+
             if main:
-                # 이미지 생성 1비트로 초기화
-                image = Image.new('1', (width, height))
-
-                # 그림 이미지드로우 객체로 가져오기
-                draw = ImageDraw.Draw(image)
-
-                # 배경 초기화
-                draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-                padding = 2
-                text_gap = 20
-
-                # 폰트 불러올거면 쓰기.
-                #font = ImageFont.truetype('파일경로/파일명.ttf', 폰트크기)
-
-                # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-                # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-                #font = ImageFont.truetype('Minecraftia.ttf', 8)
-
                 # 제품 목록 글자 적기
                 # 마지막 전인가 아닌가 확인하는 제어문
                 if btnYCount != len(data_array):
@@ -80,99 +96,102 @@ def paint():
                     # 현재 선택 출력
                     draw.text((padding, padding + text_gap * 1), "▷", font=font, fill=255)
 
-                # 디스플레이에 이미지 적용 및 출력
-                disp.image(image)
-                disp.display()
-
             if select:
-                width = disp.width
-                height = disp.height
-                image = Image.new('1', (width, height))
-                draw = ImageDraw.Draw(image)
-                draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-                padding = 2
-                text_gap = 20
-                font = ImageFont.load_default()
-
-                # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-                # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-                # font = ImageFont.truetype('Minecraftia.ttf', 8)
-
                 draw.text((15 + padding, padding + text_gap * 0), data_array[btnYCount], font=font, fill=255)
                 draw.text((15 + padding, padding + text_gap * 1), "삭제하기", font=font, fill=255)
                 draw.text((15 + padding, padding + text_gap * 2), "뒤로가기", font=font, fill=255)
-
                 draw.text((padding, padding + text_gap * btnYCount_Select), "▷", font=font, fill=255)
-
-                disp.image(image)
-                disp.display()
+            
+            # 디스플레이에 이미지 적용 및 출력
+            disp.image(image)
+            disp.display()
 
         time.sleep(1)
 
+# cen을 일단 클릭으로 설정. 무엇인지 물어봐야할 듯.
 # 버튼 스레드 작동 코드
 def btn():
-    '''
-    버튼이 어케 작동하는지 봐야함!!!!!
-    <b>중요 매우</b>
-    '''
+    while doing:
+        # 변수 초기화
+        # up, down, left, right, cen
+        stat = [0, 0, 0, 0, 0]
+        tmp_stat = [0, 0, 0, 0, 0]
+        index_stat = -1
+        diff_stat = 0
+        cur_stat = 0
 
-    # 절전모드 아닐 때,
-    # 디스플레이 상호작용
-    if ultraSonic:
-        if joyUpCheck:
-            if main:
-                if btnYCount > data_array.len() - 2:
-                    btnYCount = data_array.len() - 1
-                else:
-                    btnYCount += 1
-            if select:
-                if btnYCount_Select > 1:
-                    btnYCount_Select = 2
-                else:
-                    btnYCount_Select += 1
+        # 조이스틱 값 받아오기
+        for i in range(5):
+            cur_stat = GPIO.input(GPIO_joy[i])
 
-        if joyDownCheck:
-            if main:
-                if btnYCount < 1:
-                    btnYCount = 0;
-                else:
-                    btnYCount -= 1
-            if select:
-                if btnYCount_Select < 1:
-                    btnYCount_Select = 0;
-                else:
-                    btnYCount_Select -= 1
-        if joyClickCheck:
-            if main:
-                if(btnYCount == len(data_array) - 1):
-                    doing = False
-                elif(btnYCount != len(data_array) - 2):
-                    main = False
-                    select = True
-            if select:
-                if btnYCount_Select == 0:
-                    #제품명 선택한거. 없애는지 여부? -> 인덱스 선택 1, 2 한정걸기
-                    print("Nothing", end='')
-                if btnYCount_Select == 1:
-                    #해당 제품 삭제
-                    del data_array[btnYCount]
-                    main = True
-                    select = False
-                    btnYCount = 0
-                if btnYCount_Select == 2:
-                    #뒤로가기
-                    main = True
-                    select = False
-                    btnYCount_Select = 0
-    time.sleep(0.1)
+            if cur_stat != stat[i]:
+                tmp_stat[i] = stat[i]
+                stat[i] = cur_stat
+                if tmp_stat[i] - stat[i] > diff_stat:
+                    diff_stat = tmp_stat[i] - stat[i]
+                    # up, down, left, right, cen
+                    index_stat = i
+                if i == 5:
+                    index_stat = i
+
+        # 절전모드 아닐 때,
+        # 디스플레이 상호작용
+        if ultraSonic:
+            if index_stat == 0:
+                if main:
+                    if btnYCount > data_array.len() - 2:
+                        btnYCount = data_array.len() - 1
+                    else:
+                        btnYCount += 1
+                if select:
+                    if btnYCount_Select > 1:
+                        btnYCount_Select = 2
+                    else:
+                        btnYCount_Select += 1
+
+            if index_stat == 1:
+                if main:
+                    if btnYCount < 1:
+                        btnYCount = 0;
+                    else:
+                        btnYCount -= 1
+                if select:
+                    if btnYCount_Select < 1:
+                        btnYCount_Select = 0;
+                    else:
+                        btnYCount_Select -= 1
+
+            if index_stat == 5:
+                if main:
+                    if(btnYCount == len(data_array) - 1):
+                        doing = False
+                    elif(btnYCount != len(data_array) - 2):
+                        main = False
+                        select = True
+                if select:
+                    if btnYCount_Select == 0:
+                        #제품명 선택한거. 없애는지 여부? -> 인덱스 선택 1, 2 한정걸기
+                        print("Nothing", end='')
+                    if btnYCount_Select == 1:
+                        #해당 제품 삭제
+                        del data_array[btnYCount]
+                        main = True
+                        select = False
+                        btnYCount = 0
+                    if btnYCount_Select == 2:
+                        #뒤로가기
+                        main = True
+                        select = False
+                        btnYCount_Select = 0
+
+        time.sleep(0.5)
 
 # 기본 설정
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 '''
- 핀 설정
+ 핀, 변수 설정
 '''
 # 디스플레이
 RST = 24
@@ -188,8 +207,14 @@ GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.output(TRIG, False)
+
+# 조이스틱
+GPIO_joy = [5, 6, 16, 20, 21]
+
+for i in range(5):
+    GPIO.setup(GPIO_joy[i], GPIO.IN)
 '''
- 핀 설정 끝
+ 핀 변수 설정 끝
 '''
 
 # 대기 시간 2초
@@ -200,24 +225,34 @@ time.sleep(0.5)
 print(".", end='')
 time.sleep(1)
 
-# y축 조작 횟수
-btnYCount = 0
-btnYCount_Select = 0
-
 # 디스플레이 기본설정
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 disp.begin()
 disp.clear()
 disp.display()
 
+# 사용하는 변수 초기화
+idleTime = 5
+doing = True
+ultraSonic = False
+joyUpCheck = False
+joyDownCheck = False
+joyClickCheck = False
+main = True # 메인, 선택
+select = False
+btnYCount = 0 # y축 조작 횟수
+btnYCount_Select = 0
+file_path = '1.txt' # 파일경로
+
 #텍스트 뽑아내기
-file_path = '파일경로/파일명.확장자'
 with open(file_path, 'r', encoding='utf-8') as file:
     lines = file.readlines()
 
 data_array = []
+
 for line in lines:
     data_array.append(line.strip())
+# 인덱스 2개 추가댐. 삭제에서 2개 삭제함.
 data_array.append('----------')
 data_array.append('끝내기')
 
@@ -228,14 +263,6 @@ disp_t.start()
 # 버튼 스레드
 btn_t = threading.Thread(target=btn)
 btn_t.start()
-
-# 사용하는 변수 초기화
-idleTime = 5
-doing = True
-ultraSonic = False
-joyUpCheck = False
-joyDownCheck = False
-joyClickCheck = False
 
 # 무한 반복 코드
 # 라즈베리파이 가동 후, 종료까지 여기서 갇힘.
@@ -268,3 +295,12 @@ while doing:
     
     # 초음파센서 2초 딜레이 필요.
     time.sleep(2)
+
+# GPIO 정리
+GPIO.cleanup()
+
+# 값 저장하기
+data_array = data_array[:-2] # 인덱스 뒤에 2개 삭제
+with open(file_path, 'w', encoding='utf-8') as output_file:
+    for item in data_array:
+        output_file.write("%s\n" % item)
